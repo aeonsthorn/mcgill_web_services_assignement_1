9140b81f272d7a7625fb603aaa55ce5ba01d9023
Simon <sleroux@carboniaweb.com>
2023-09-23 21:05:44 -0400

    WIP add http method logic + move get movie by id to method

--- src/main/java/org/example/MovieServlet.java
+++ src/main/java/org/example/MovieServlet.java
@@ -1,23 +1,37 @@
 package org.example;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
 import java.util.ArrayList;
 
 public class MovieServlet extends HttpServlet {
 
     ArrayList<Movie> movies = new ArrayList<>();
+
+    public MovieServlet() {
+
+        Movie batman = new Movie();
+        batman.setImdbId("abc");
+        Movie pumpingIron = new Movie();
+        pumpingIron.setImdbId("def");
+
+        this.movies.add(batman);
+        this.movies.add(pumpingIron);
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 
         String movieId = req.getParameter("id");
 
-        if(movieId == null){
+        if (movieId == null) {
             String strMovies = "[";
 
-            for(int i = 0 ; i < this.movies.size(); i++){
+            for (int i = 0; i < this.movies.size(); i++) {
                 strMovies += this.movies.get(i).toString();
             }
 
@@ -27,11 +41,11 @@ public class MovieServlet extends HttpServlet {
         }
 
 
-        for(int i = 0 ; i < this.movies.size(); i++){
+        for (int i = 0; i < this.movies.size(); i++) {
 
             Movie movie = this.movies.get(i);
 
-            if(movie.getImdbId().equals(movieId)){
+            if (movie.getImdbId().equals(movieId)) {
 
                 resp.getWriter().write(movie.toString());
                 return;
@@ -45,7 +59,9 @@ public class MovieServlet extends HttpServlet {
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException,IOException{
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        System.out.println(req.getParameter("ImdbId"));
 
         Movie movie = new Movie();
 
@@ -59,33 +75,66 @@ public class MovieServlet extends HttpServlet {
 
 
     @Override
-    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws IOException{
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+        String movieId = req.getParameter("id");
+
+        Movie movie = this.getMovieById(movieId);
 
 
-        resp.getWriter().write("Hello put");
+        if(movie == null){
+            resp.setStatus(404);
+            return;
+
+        }
+
+        movie.setDescription("Coucou");
+
+
+
+        resp.getWriter().write("Ok");
+
     }
 
 
     @Override
-    protected  void doDelete(HttpServletRequest req, HttpServletResponse resp) throws IOException{
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 
         String movieId = req.getParameter("id");
 
-        for(int i = 0 ; i < this.movies.size(); i++){
+        Movie movie = this.getMovieById(movieId);
+
+        if(movie == null){
+            resp.setStatus(404);
+            return;
+
+        }
+
+
+        this.movies.remove(movie);
+
+        resp.getWriter().write("Ok");
+
+
+
+
+    }
+
+
+    private Movie getMovieById(String id) {
+
+        for (int i = 0; i < this.movies.size(); i++) {
 
             Movie movie = this.movies.get(i);
 
-            if(movie.getImdbId().equals(movieId)){
+            if (movie.getImdbId().equals(id)) {
 
-                this.movies.remove(i);
-                return;
+                return movie;
 
             }
         }
 
-
-        resp.setStatus(404);
-
+        return null;
 
     }
 }
